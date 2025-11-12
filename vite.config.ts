2b26dbd96104e6871fee88cb679ea4392404e81d
gabrdsp <paulo.paiva@alunos.ufersa.edu.br>
2025-11-12 18:19:25 -0300

    Vite update.

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/CILab-Navigation/', 
+  base: '/CILab-Navigation/',
   server: {
     port: 5173,
     open: true,
